d1f9357b242428a90dfd7393ef742b6e52d326d3
Hwankyu Jhun <h.jhun@samsung.com>
2015-12-21 14:03:28 +0900

    Add bg uiapp limit feature
    - If the value of DEVOPTION_BGPROCESS vconfkey is greater than zero,
    AMD will kill the background processes that are managed by
    task-manager.
    - Before killing the processes, AMD will compare the timestamp and the
    number of processes, the number of foreground.
    
    Change-Id: Iedf2c68335073465b574f2764c56c266cf79affe
    Signed-off-by: Hwankyu Jhun <h.jhun@samsung.com>

--- src/amd_launch.c
+++ src/amd_launch.c
@@ -541,7 +541,6 @@ static int __get_pid_for_app_group(const char *appid, int pid, int caller_uid, b
 		st = _status_get_app_info_status(pid, caller_uid);
 
 	if (pid == -1 || st == STATUS_DYING) {
-
 		if (app_group_find_singleton(appid, &found_pid, &found_lpid) == 0) {
 			pid = found_pid;
 			*new_process = FALSE;
@@ -716,7 +715,7 @@ int _start_app(const char* appid, bundle* kb, int cmd, int caller_pid,
 	char tmpbuf[MAX_PID_STR_BUFSZ];
 	const char *hwacc;
 	char *caller_appid;
-	int lpid;
+	int lpid = -1;
 	int callee_status = -1;
 	gboolean can_attach = FALSE;
 	gboolean new_process = FALSE;
@@ -829,7 +828,7 @@ int _start_app(const char* appid, bundle* kb, int cmd, int caller_pid,
 	}
 
 	if (pid > 0) {
-		_status_add_app_info_list(appid, app_path, pid, LAUNCHPAD_PID, is_subapp, caller_uid);
+		_status_add_app_info_list(appid, app_path, pid, LAUNCHPAD_PID, is_subapp, lpid, caller_uid);
 		if (strncmp(component_type, APP_TYPE_UI, strlen(APP_TYPE_UI)) == 0) {
 			if (new_process) {
 				_D("add app group info");
