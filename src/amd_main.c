35c0c1ae689a5d6ec9cd89951a64c421d24b3a44
Sangyoon Jang <s89.jang@samsung.com>
2016-01-05 11:58:37 +0900

    Initialize cynara and rua at main initialize func
    Change-Id: I82ef8c8d1ad1b1dc5f35c200a61e4c7cce5c8bef
    Signed-off-by: Sangyoon Jang <s89.jang@samsung.com>

--- src/amd_main.c
+++ src/amd_main.c
@@ -22,6 +22,7 @@
 #include <fcntl.h>
 #include <string.h>
 #include <aul.h>
+#include <rua.h>
 #include <pkgmgr-info.h>
 #include <glib.h>
 #include <stdlib.h>
@@ -39,6 +40,7 @@
 #include "amd_launch.h"
 #include "amd_request.h"
 #include "amd_app_group.h"
+#include "amd_cynara.h"
 
 #define GLOBAL_USER tzplatform_getuid(TZ_SYS_GLOBALAPP_USER)
 #define AUL_SP_DBUS_PATH "/Org/Tizen/Aul/Syspopup"
@@ -270,6 +272,8 @@ static int __syspopup_dbus_signal_handler_init(void)
 
 static int __init(void)
 {
+	int r;
+
 	if (appinfo_init()) {
 		_E("appinfo_init failed\n");
 		return -1;
@@ -281,9 +285,20 @@ static int __init(void)
 	}
 
 	restart_tbl = g_hash_table_new(g_str_hash, g_str_equal);
+
+	r = init_cynara();
+	if (r != 0) {
+		_E("cynara initialize failed.");
+		return -1;
+	}
+
 	_request_init();
 	_status_init();
 	app_group_init();
+	r = rua_init();
+	r = rua_clear_history();
+
+	_D("rua_clear_history : %d", r);
 
 	if (__syspopup_dbus_signal_handler_init() < 0)
 		 _E("__syspopup_dbus_signal_handler_init failed");
