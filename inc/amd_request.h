ae26dce9bbcde3eb4e93d2e178b7a9b2c4619276
Daehyeon Jung <darrenh.jung@samsung.com>
2016-02-01 16:44:11 +0900

    Use bundle-wrapped aul_sock APIs
    - Refine aul_sock handling on AMD
    
    Change-Id: I2e510ad9f77f60d8ccd2a063877a5ecf605c8972

--- inc/amd_request.h
+++ inc/amd_request.h
@@ -16,7 +16,17 @@
 
 #pragma once
 
+typedef struct request_s* request_h;
+
 int _request_init(void);
+int _request_send_result(request_h req, int res);
+int _request_send_raw(request_h req, int cmd, unsigned char *data, int len);
+int _request_get_fd(request_h req);
+int _request_get_pid(request_h req);
+int _request_get_cmd(request_h req);
+request_h _request_create_local(int cmd, int uid, int pid);
+void _request_free_local(request_h req);
+int _request_remove_fd(request_h req);
 int _request_reply_for_pending_request(int pid);
 int _request_flush_pending_request(int pid);
 
