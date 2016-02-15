113624d07b66404fb0a1793586621a3f625ef6a6
Hwankyu Jhun <h.jhun@samsung.com>
2016-02-15 21:01:25 +0900

    Support splash screen
    If an ui-application has the splash images, the AMD will send
    the splash screen information to the window manager before sending
    the request to the launchpad to execute the application.
    
    Requires:
    https://review.tizen.org/gerrit/#/c/59353/
    https://review.tizen.org/gerrit/#/c/59354/
    https://review.tizen.org/gerrit/#/c/59363/
    
    Change-Id: Ie85300c809bb797407fdec5e74f654bf5cd3bd0a
    Signed-off-by: Hwankyu Jhun <h.jhun@samsung.com>

---
+++ inc/amd_splash_screen.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <glib.h>
+#include <bundle.h>
+#include <tizen-extension-client-protocol.h>
+
+#include "amd_appinfo.h"
+
+typedef struct splash_image* splash_image_h;
+
+splash_image_h _splash_screen_create_image(const struct appinfo *ai,
+				bundle *kb, int cmd);
+void _splash_screen_send_image(splash_image_h si);
+void _splash_screen_send_pid(splash_image_h si, int pid);
+void _splash_screen_destroy_image(splash_image_h si);
+int _splash_screen_init(void);
